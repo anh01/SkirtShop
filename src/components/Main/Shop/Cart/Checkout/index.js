0e6d939fd4bcac488c6c48157bbdf21e886ef67c
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-04 09:37:16 +0700

    finish shop creating components

--- src/components/Main/Shop/Cart/Checkout/index.js
+++ src/components/Main/Shop/Cart/Checkout/index.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import global from '../../../../global';
 
 export default class Checkout extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.checkout}>
                 <Text>Check Out Component</Text>
                 <TouchableOpacity onPress={() => this.props.navigator.pop()}>
                     <Text>Go back cart detail</Text>
                 </TouchableOpacity>
-                <TouchableOpacity onPress={() => global.goToHome()}>
+                <TouchableOpacity 
+                    onPress={() => { 
+                        global.goToHome(); 
+                        this.props.navigator.pop();
+                    }}
+                >
                     <Text>Go Home</Text>
                 </TouchableOpacity>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    checkout: {
+        backgroundColor: '#B8B8B8',
+        flex: 1
+    }
+});
