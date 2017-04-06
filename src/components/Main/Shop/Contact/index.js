beb86a33abc0053bbcf6eca6b72bfe9a1274b39a
Van Pho Nguyen <vanpho02@gmail.com>
2017-04-06 09:53:02 +0700

    contact finished

--- src/components/Main/Shop/Contact/index.js
+++ src/components/Main/Shop/Contact/index.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Image, Dimensions,TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Dimensions } from 'react-native';
 import map from '../../../../media/appIcon/map.png';
-import profileImage from '../../../../media/appIcon/teller.png';
+
+import phoneIcon from '../../../../media/appIcon/phone.png';
+import mailIcon from '../../../../media/appIcon/mail.png';
+import messageIcon from '../../../../media/appIcon/message.png';
+import locationIcon from '../../../../media/appIcon/location.png';
 
 class Contact extends Component {
     render() {
         const { 
             mapStyle, mapContainer, wrapper, infoContainer, 
-            addressStyle, contactHover, profileStyle, addressContainer,
+            rowInfoContainer, imageStyle, infoText
         } = styles;
         return (
             <View style={wrapper}>
@@ -15,22 +19,21 @@ class Contact extends Component {
                     <Image source={map} style={mapStyle} />
                 </View>
                 <View style={infoContainer}>
-                    <View style={addressContainer}>
-                        <Text style={addressStyle}>Adress: 90 Le Thi Rieng, Ben Thanh</Text>
+                    <View style={rowInfoContainer}>
+                       <Image source={locationIcon} style={imageStyle} />
+                       <Text style={infoText}>90 Le Thi Rieng/ Ben Thanh Dist</Text>
                     </View>
-                    <View style={contactHover}>
-                        <View>
-                            <Text>(+84) 1694472176</Text>
-                            <Text>Home phone</Text>
-                        </View>
-                        <View>
-                            <TouchableOpacity>
-                                <Image />
-                            </TouchableOpacity>
-                            <TouchableOpacity>
-                                <Image />
-                            </TouchableOpacity>
-                        </View>
+                    <View style={rowInfoContainer}>
+                       <Image source={messageIcon} style={imageStyle} />
+                       <Text style={infoText}>(+84) 01694472176</Text>
+                    </View>
+                    <View style={rowInfoContainer}>
+                       <Image source={mailIcon} style={imageStyle} />
+                       <Text style={infoText}>khoaphamtraining@gmail.com</Text>
+                    </View>
+                    <View style={[rowInfoContainer, { borderBottomWidth: 0 }]}>
+                       <Image source={messageIcon} style={imageStyle} />
+                       <Text style={infoText}>(+84) 09877067707</Text>
                     </View>
                 </View>
             </View>
@@ -41,7 +44,13 @@ class Contact extends Component {
 const { width } = Dimensions.get('window');
 const styles = StyleSheet.create({
     wrapper: { flex: 1, backgroundColor: '#F6F6F6' },
-    mapStyle: { width: width - 40, height: 230, alignSelf: 'center', alignItems: 'center', justifyContent: 'center' },
+    mapStyle: { 
+        width: width - 40, 
+        height: 230, 
+        alignSelf: 'center', 
+        alignItems: 'center', 
+        justifyContent: 'center' 
+    },
     mapContainer: { 
         justifyContent: 'center',
         alignItems: 'center',
@@ -64,27 +73,22 @@ const styles = StyleSheet.create({
         shadowOffset: { width: 0, height: 3 },
         shadowOpacity: 0.2
     },
-    addressContainer: {
-        flex: 1
+    rowInfoContainer: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderColor: '#D6D6D6'
     },
-    addressStyle: {
-        fontSize: 20,
+    imageStyle: {
+        width: 30,
+        height: 30
+    },
+    infoText: {
         fontFamily: 'Avenir',
-        color: '#39B38F'
-    },
-    contactHover: {
-        flex: 2,
-        justifyContent: 'center',
-        backgroundColor: '#39B38F'
-    },
-    imageContainer: {
-        width: 100,
-        height: 100,
-    },
-    profileStyle: {
-        width: 70,
-        height: 70,
-        borderRadius: 35
+        color: '#AE005E',
+        fontWeight: '500'
     }
 });
 
